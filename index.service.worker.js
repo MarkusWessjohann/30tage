0a66c5e2641e52b8290a4090e62ca247330afd82
Markus Wessjohann <markus.wessjohann@posteo.de>
2026-09-08 13:23:01 +0200

    0.9: export web build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788846484|16205231647';
+const CACHE_VERSION = '1788938208|5018654';
 /** @type {string} */
 const CACHE_PREFIX = '30 Tage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
